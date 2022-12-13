514c7ed94e001fc889ebfb250218b4780861b01c
Anthony Campos <Anthonyck89@hotmail.com>
2022-12-13 14:41:09 -0800

    break today

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,11 +1,13 @@
 "use strict";
 
-const calcDisplay = document.querySelector(".calc__text");
+/** Application  Architecture*/
 
+const calcDisplay = document.querySelector(".calc__text");
 const calcPad = document.querySelector(".calc__pad");
 
 class calculator {
   #curInput;
+  #curExpression;
   #calcResult;
   #calcHistory = [];
   constructor() {
@@ -14,18 +16,20 @@ class calculator {
 
   _getInput() {
     // process input from calc pad
-    calcPad.addEventListener("click", function (event) {
-      // guard clause
-      if (event.target.classList === "calc__pad") {
-        return;
-      } // end if
-    });
+    calcPad.addEventListener("click", this._processBtnInput.bind(this));
 
     // process input from user's keyboard
-    addEventListener("keydown", this._iskeyValid);
+    addEventListener("keydown", this._iskeyValid.bind(this));
   }
 
-  _processBtnInput(event) {}
+  _processBtnInput(event) {
+    if (event.target.classList === "calc__pad") {
+      return;
+    } // end if
+
+    const inputVal = event.target.value;
+    this._inputDelegatory(inputVal);
+  }
 
   _processKeyInput() {}
 
@@ -33,11 +37,40 @@ class calculator {
     console.log(`hi`);
   }
 
-  _inputDelegatory() {}
+  _inputDelegatory(inputVal) {
+    // check if button pressed is a number
+    if (isFinite(inputVal)) {
+      // first check value currently in display
+      let curVal = calcDisplay.textContent;
+      // if zero replace
+      if (Number(curVal) === 0) {
+        curVal = inputVal;
+      } else if (Number(curVal) !== 0) {
+        curVal += inputVal;
+      }
+      calcDisplay.textContent = curVal;
+      // set the value in the calcDisplay
+    } else {
+      // process operator or cmd
+    }
+  }
 
+  _commandMap(inputVal) {
+    const cmdMap = new Map(
+      ["clear all", this._clearAll],
+      ["clear", this._clear]
+    );
+
+    const cmd = cmdMap.get(inputVal);
+  }
   _storeOperator() {}
 
   _calcInput() {}
+
+  _clear() {
+    calcDisplay.textContent = 0;
+  }
+  _clearAll() {}
 
   _updateDisplayInput() {}
 } // end of calculator
