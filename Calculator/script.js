be5adcc9029e0e125364c7e1ef777ab0f9a662f6
Anthony Campos <Anthonyck89@hotmail.com>
2022-12-15 09:03:33 -0800

    added math functions

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,17 +3,19 @@
 /** Application  Architecture*/
 
 const calcDisplay = document.querySelector(".calc__text");
-const calcDisplayPrev = document.querySelector(".calc__prev__text");
+const calcDisplayExpress = document.querySelector(".calc__prev__text");
 const calcPad = document.querySelector(".calc__pad");
 
 class calculator {
   #curInput;
-  #curExpression;
+  #curExpression = ["0"];
+  #curExpPos = 0;
   #calcResult;
   #calcHistory = [];
   #cmdMap = new Map([
     ["clear", this._clear],
     ["clear entry", this._clearEntry],
+    ["=", this._calcInput],
   ]);
 
   constructor() {
@@ -47,13 +49,51 @@ class calculator {
     const operators = ["/", "*", "-", "+"];
     // check if button pressed is a number or decimal
     if (isFinite(inputVal) || inputVal === ".") {
-      const curVal = calcDisplay.textContent;
-      calcDisplay.textContent = this._validateOprend(curVal, inputVal);
+      const curVal = this.#curExpression[this.#curExpPos];
+      this.#curExpression[this.#curExpPos] = this._validateOprend(
+        curVal,
+        inputVal
+      );
+      calcDisplay.textContent = this.#curExpression[this.#curExpPos];
       // set the value in the calcDisplay
-    } else {
-      this._commandMap(inputVal);
     }
+
+    if (operators.includes(inputVal)) {
+      // so if operator is 1st or 2nd input and is not already in expression
+
+      if (
+        !this._hasOperator(operators, inputVal) &&
+        this.#curExpression.length <= 2
+      ) {
+        let curPos = this.#curExpPos;
+        this.#curExpression[++curPos] = inputVal;
+        calcDisplayExpress.textContent = this.#curExpression.join(" ");
+        curPos++;
+        this.#curExpression[curPos] = "0";
+        this.#curExpPos = curPos;
+        console.log(this.#curExpression);
+        // update current expression display
+      }
+      // if operator is 4th input process expression
+      if (this.#curExpression.length === 3) {
+        // we solve the expression using the command
+        // save the result as the first value, and add the operator to the expression
+      }
+      // if prev el is operator ignore
+    }
+
+    // we can just run the commmand at the end for now
+    this._commandMap(inputVal);
   }
+
+  _hasOperator(operators, inputVal) {
+    this.#curExpression.forEach((el) => {
+      if (operators.findIndex((op) => op === el) > -1) {
+        return true;
+      }
+    });
+    return false;
+  } // end _hasOperator
 
   _validateOprend(val, inputVal) {
     if (val === "0" && inputVal !== ".") {
@@ -72,17 +112,49 @@ class calculator {
   } // end
 
   _commandMap(inputVal) {
-    this.#cmdMap.get(inputVal)();
+    const cmd = this.#cmdMap.get(inputVal)?.bind(this);
+    if (!cmd) {
+      return;
+    }
+    cmd();
   }
   _storeOperator() {}
 
-  _calcInput() {}
+  _calcInput() {
+    const opMap = new Map([
+      ["+", bigDecimal.add],
+      ["-", bigDecimal.subtract],
+      ["*", bigDecimal.multiply],
+      ["/", bigDecimal.divide],
+    ]);
+    // index 1 should always be the operator
+    let expression = this.#curExpression;
+    const [oprendL, operator, oprendR] = expression;
+    const result = opMap.get(operator)(oprendL, oprendR);
+    calcDisplay.textContent = result;
+    calcDisplayExpress.textContent = expression.join(" ") + "=";
+    expression = [String(result)];
+    this.#curExpression = expression;
+    // will need to look into this
+    // The MS calc will continue using the result unless you enter a number
+
+    // possible solution don't store the first num right away
+    // until we get an operator
+    this.#curExpPos = 0;
+    console.log(this.#curExpression);
+
+    // will need to update for when the user just hits equal with
+    // no operator
+    //return opMap.get(operator)(oprendL, oprendR);
+  }
 
   _clear() {
-    calcDisplay.textContent = 0;
-    calcDisplayPrev.textContent = "";
+    this.#curExpression = ["0"];
+    calcDisplay.textContent = this.#curExpression[0];
+    calcDisplayExpress.textContent = "";
   }
   _clearEntry() {
+    this.#curExpression[this.#curExpPos] = "0";
     calcDisplay.textContent = 0;
   }
 
